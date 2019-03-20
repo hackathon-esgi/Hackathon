0ddbac37e1a5ffcd8b5f49ee7af703a0b0bd13a2
muhammad-tounsi <muhammad.tounsi@hotmail.fr>
2019-03-20 21:59:57 +0100

    Api Platform and entities

---
+++ api-platform/api/src/Entity/KeyWord.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\KeyWordRepository")
+ */
+class KeyWord
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbArticle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getNbArticle(): ?int
+    {
+        return $this->nbArticle;
+    }
+
+    public function setNbArticle(int $nbArticle): self
+    {
+        $this->nbArticle = $nbArticle;
+
+        return $this;
+    }
+}
---
+++ api-platform/api/src/Entity/SubCategory.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\SubCategoryRepository")
+ */
+class SubCategory
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $scoreSociale;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbArticle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="subCategories")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScoreSociale(): ?int
+    {
+        return $this->scoreSociale;
+    }
+
+    public function setScoreSociale(int $scoreSociale): self
+    {
+        $this->scoreSociale = $scoreSociale;
+
+        return $this;
+    }
+
+    public function getNbArticle(): ?int
+    {
+        return $this->nbArticle;
+    }
+
+    public function setNbArticle(int $nbArticle): self
+    {
+        $this->nbArticle = $nbArticle;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
